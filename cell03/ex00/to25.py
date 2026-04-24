06d77cff8a7b87269d6f5145a31b8ce99ee75ab3
win108 <kwinvarin2@gmail.com>
2026-04-24 15:14:46 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+print("Enter a number less than 25 ")
+num = int(input())
+if num<26 :
+    while num<26:
+        print("Inside the loop, my variable is",num)
+        num+=1
+else:
+    print("Error")
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+ 
+if len(sys.argv)>1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i<11 :
+    line = "Table de " + str(i) + ":"
+    j=0
+    while j<11:
+        line += " "+str(j*i)
+        j+=1
+
+    print(line)
+    i +=1
